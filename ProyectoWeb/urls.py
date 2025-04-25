5fee747ec59c19a06f72524228e852d47b619b88
AgustinaTarifa <agusstarifa@gmail.com>
2025-04-24 21:04:12 -0300

    app Contacto

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('servicios/', include('servicios.urls')),
     path('blog/', include('blog.urls')),
+    path('contacto/', include('contacto.urls')),
     path('',include('ProyectoWebApp.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
